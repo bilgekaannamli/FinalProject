d82373f185330195d85f3fc6252208ea0f7dfb6a
BKN <bilgekaannamli@gmail.com>
2021-01-31 00:25:23 +0300

    Add project files.

---
+++ DataAccess/Abstract/IProductDal.cs
@@ -0,0 +1,21 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    //DAL: Data Access Layer
+    public interface IProductDal
+    {
+        //Aşağıdaki methodlar default olarak "public"tir.
+
+        //Entities altındaki Product class'ını çağırabilmek için önce Entities refere edilir,
+        //ardından Concrete yukarıya eklenir.
+        List<Product> GetAll();
+        void Add(Product product);
+        void Update(Product product);
+        void Delete(Product product);
+        List<Product> GetAllByCategory(int categoryId);
+    }
+}
